84930bedeedc1541c1ec3745b28662eb3ce1be99
sameemul-haque <samhaqk@gmail.com>
2026-09-13 10:13:49 +0530

    feat: move examples to their own page

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,10 @@ import { initCalendar } from "./calendar.js";
 import { initTextDraw, redrawText, clearGraph } from "./text-draw.js";
 import { displayCode, copyCode, generateShellScript } from "./commands.js";
 import { githubJoinDate } from "./github.js";
-import { openExamples, closeExamples } from "./examples.js";
 
 initCalendar(redrawText);
 initTextDraw();
 
-document.getElementById("open-examples").addEventListener("click", openExamples);
-document.getElementById("close-examples").addEventListener("click", closeExamples);
 document.getElementById("join-date").addEventListener("click", githubJoinDate);
 document.getElementById("clear-graph").addEventListener("click", clearGraph);
 document.getElementById("generate-commands").addEventListener("click", displayCode);
